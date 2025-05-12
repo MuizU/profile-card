8cb94870ae68e514383bb8120280ab4852e86815
muizu <muizuvais99@gmail.com>
2025-05-12 17:10:58 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,41 @@
-import Image from "next/image";
+import Card from "@/app/components/Card";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const description =
+    "I transform caffeine into clean, elegant code that powers innovation and rarely breaks. Trusted contributor on Stack Overflow.";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
+  return (
+    <article className={styles.container}>
+      <Card
+        className={styles.card}
+        description={description}
+        emailId="mailto:muizuvais99@gmail.com"
+        name="Muiz Uvais"
+        role="Software Development Engineer II"
+        org="noon.com"
+        ghId="https://github.com/MuizU"
+        liId="https://www.linkedin.com/in/muizuvais/"
+        igId="http://instagram.com/muiz_developer"
+        xId="http://x.com/muiz_the_dev"
+      />
+      <div className={styles.credits} data-gfe-screenshot-exclude="true">
+        A challenge by <s></s>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.greatfrontend.com/projects?ref=challenges"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          GreatFrontEnd Projects
         </a>
+        . Built by <s />
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.greatfrontend.com/projects/u/MuizU"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          Muiz Uvais
         </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        .
+      </div>
+    </article>
   );
 }
